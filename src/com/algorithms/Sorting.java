d841af37d737fe414403ce82348a36a6d639d839
Raghav <raghav.tanaji@salesforce.com>
2021-07-28 10:07:50 +0530

    Add comments to the Insertion Sort

--- src/com/algorithms/Sorting.java
+++ src/com/algorithms/Sorting.java
@@ -4,16 +4,17 @@ public class Sorting {
 
     public Integer[] insertionSort(Integer[] array){
 
-        for( int i = 1 ;i < array.length; i++)
+        // for empty array or array with ONLY 1 element it is already sorted
+        for( int i = 1 ;i < array.length; i++)     /// Start from index = 1 element
         {
-            Integer key = array[i];
-            Integer j = i-1;
-            while(j>=0 && array[j]>key)
+            Integer key = array[i];                /// make the element at index = i as KEY
+            Integer j = i-1;                       /// KEY can be any where Array[0] <= KEY <= Array[i]
+            while(j>=0 && array[j]>key)            /// Loop till j >= 0 and Array[j] > KEY i.e KEY is left of Array[j]
             {
-                array[j+1] = array[j];
-                j--;
+                array[j+1] = array[j];             /// As we need to move all the elements backwards and insert the KEY at its right position
+                j--;                               /// update Array[j+1] to Array[j]
             }
-            array[j+1] = key;
+            array[j+1] = key;                      /// At this point we found the position of KEY in the already SORTED Array insert the element.
         }
 
         return  array;
